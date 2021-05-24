53e9c8f07f3779bba44a86bf7bfebe13bf088272
thebl <thebl@DESKTOP-BOJIPQC>
2021-05-24 11:36:49 -0700

    board with pieces

--- Chess/src/Driver.java
+++ Chess/src/Driver.java
@@ -1,11 +1,26 @@
-import java.awt.*;
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.*;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 public class Driver extends JPanel{
 
+	BufferedImage knight;
+	BufferedImage rook;
+	BufferedImage bishop;
+	BufferedImage pawn;
+	
+	
 	public void paint(Graphics g) {
 		int a = 1;
 		Color c;
@@ -25,13 +40,34 @@ public class Driver extends JPanel{
 			}
 			a++;
 		}
+	   
+	    g.drawImage(knight, 100, 700, 100, 100, this);
+	    g.drawImage(knight,  600,  700, 100, 100,  this);
+	    g.drawImage(rook,  0,  700,  100, 100, this);
+	}
+	
+	public Driver() {
+				knight = createImg("images/knight.png");
+				rook = createImg("images/rook.png");
+				bishop = createImg("images/bishop.png");
+				pawn = createImg("images/pawn.png");
+	}
+	
+	public BufferedImage createImg(String a) {
+		try {
+			return ImageIO.read(new File(a));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Chess");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel textLabel = new JLabel("I'm a label in the window", SwingConstants.CENTER);
+		JLabel textLabel = new JLabel("WEEWOOOOOOOOOOOOO", SwingConstants.CENTER);
 		frame.setLayout(new GridLayout());
 		frame.setSize(800, 800);
 		
@@ -40,6 +76,9 @@ public class Driver extends JPanel{
 		frame.setBackground(Color.LIGHT_GRAY);
 		
 		frame.setVisible(true);
+		
+
 	}
+	
 
 }
